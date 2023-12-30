58e9a4742237cd4efd52d599a5f494c9280b0e0e
Kelechikizito <kaylaychi77@gmail.com>
2023-12-30 11:09:10 +0100

    Second commit, HTML, CSS and JS update

--- index.js
+++ index.js
@@ -5,38 +5,21 @@ const email = $("input[name='email']");
 const password = $("input[name='password']");
 const btn = $('button');
 
+const isFNameValid = firstName[0].validity;
+const isLNameValid = lastName[0].validity;
+const isEmailValid = email[0].validity;
+const isPasswordValid = password[0].validity;
 
-btn.click(function (e) { 
+// const validateFname = () => {
 
-    const isFNameValid = firstName[0].checkValidity();
-    const isLNameValid = lastName[0].checkValidity();
-    const isEmailValid = email[0].checkValidity();
-    const isPasswordValid = password[0].checkValidity();
+// }
+
+btn.click(function (e) {
     
-    if (!isFNameValid && !isLNameValid && !isEmailValid && !isPasswordValid) {
-        firstName.addClass('invalid-input');
-        firstName.attr('placeholder', '');
-        lastName.addClass('invalid-input');
-        lastName.attr('placeholder', '');
-        email.addClass('invalid-input update-placeholder');
-        email.attr('placeholder','name@host.tld');
-        password.addClass('invalid-input');
-        password.attr('placeholder', '');
-    }else if (!isFNameValid) {
-        firstName.addClass('invalid-input');
-        firstName.attr('placeholder', '');
-    }else if (!isLNameValid) {
-        lastName.addClass('invalid-input');
-        lastName.attr('placeholder', '');
-    }else if (!isEmailValid) {
-        email.addClass('invalid-input update-placeholder');
-        email.attr('placeholder','name@host.tld');
-    }else if (!isPasswordValid) {
-        password.addClass('invalid-input');
-        password.attr('placeholder', '');
-    }
-    if (isFNameValid && isLNameValid && isEmailValid && isPasswordValid) {
+    console.log(isFNameValid);
+    console.log(isLNameValid);
+    console.log(isEmailValid);
+    console.log(isPasswordValid);
 
-    }
-    
+    e.preventDefault();
 });
