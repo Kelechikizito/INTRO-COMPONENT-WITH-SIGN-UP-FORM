d867bf329937ad3dce7323dc4c37430b4ee91c9e
Kelechikizito <kaylaychi77@gmail.com>
2024-01-05 06:22:44 +0100

    JS, CSS and HTML Update, ReadME file update, Screenshots attach

--- index.js
+++ index.js
@@ -1,25 +1,77 @@
 
+const form = $('form');
 const firstName = $("input[name='firstName']");
 const lastName = $("input[name='lastName']");
 const email = $("input[name='email']");
 const password = $("input[name='password']");
 const btn = $('button');
+const firstErr = $('.first');
+const secondErr = $('.second');
+const thirdErr = $('.third');
+const fourthErr = $('.fourth');
 
-const isFNameValid = firstName[0].validity;
-const isLNameValid = lastName[0].validity;
-const isEmailValid = email[0].validity;
-const isPasswordValid = password[0].validity;
+function validateFName() {
+    const isFNameValid = firstName[0].validity.valid;
 
-// const validateFname = () => {
+    if (!isFNameValid) {
+        firstErr.css('display', 'inline-block');
+        firstName.addClass('invalid-input');
+        firstName.removeAttr('placeholder');
+    } else {
+        firstErr.css('display', 'none');
+        firstName.removeClass('invalid-input');
+        firstName.attr('placeholder', 'First Name');
+    }
+}
 
-// }
+function validateLName() {
+     const isLNameValid = lastName[0].validity.valid;
 
-btn.click(function (e) {
-    
-    console.log(isFNameValid);
-    console.log(isLNameValid);
-    console.log(isEmailValid);
-    console.log(isPasswordValid);
+     if (!isLNameValid) {
+        secondErr.css('display', 'inline-block');
+        lastName.addClass('invalid-input');
+        lastName.removeAttr('placeholder');
+     } else {
+        secondErr.css('display', 'none');
+        lastName.removeClass('invalid-input');
+        lastName.attr('placeholder', 'Last Name');
+     }
+}
 
-    e.preventDefault();
+function validateEmail() {
+    const isEmailValid = email[0].validity.valid;
+
+    if (!isEmailValid) {
+        thirdErr.css('display', 'inline-block');
+        email.addClass('invalid-input');
+        email.attr('placeholder', 'name@host.tld');
+        email.addClass('invalid-email');
+    } else {
+        thirdErr.css('display', 'none');
+        email.removeClass('invalid-input invalid-email');
+    }
+}
+
+function validatePassword() {
+    const isPasswordValid = password[0].validity.valid;
+
+    if (!isPasswordValid) {
+        fourthErr.css('display', 'inline-block');
+        password.addClass('invalid-input');
+        password.removeAttr('placeholder');
+    } else {
+        fourthErr.css('display', 'none');
+        password.removeClass('invalid-input');
+        password.attr('placeholder', 'Password');
+    }
+}
+
+btn.click(function (e) { 
+    if (!form[0].checkValidity()) {
+        validateFName();
+        validateLName();
+        validateEmail();
+        validatePassword();
+        e.preventDefault();     
+    } 
 });
